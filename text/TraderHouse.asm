e461ca0ab8df7eb7884c636430bbbe6246e4557d
Derek Andersen <derekcandersen@gmail.com>
2024-10-24 10:59:34 -0400

    modify Trader journal text slightly

--- text/TraderHouse.asm
+++ text/TraderHouse.asm
@@ -1,9 +1,9 @@
 _TraderJournalText::
- 	text "MACHOKE,"
-     para "KADABRA,"
-     para "HAUNTER,"
-     para "GRAVELER."
-     para "So far, these are"
-     line "the only ones I've"
-     para "come across."
- 	done
+	text "MACHOKE."
+    para "KADABRA."
+    para "HAUNTER."
+    para "GRAVELER."
+    para "So far, these are"
+    line "the only ones I've"
+    para "come across."
+	done
