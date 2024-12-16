350168b5ad10aa67fd1e7c02b3d4ada1daf8de14
Rangi42 <sylvie.oukaour+rangi42@gmail.com>
2024-12-16 10:35:55 -0500

    Remove redundant parentheses

--- engine/menus/main_menu.asm
+++ engine/menus/main_menu.asm
@@ -237,7 +237,7 @@ LinkMenu:
 	ld c, " "
 	ld d, "▷"
 	ld a, [wLinkMenuSelectionSendBuffer]
-	and (B_BUTTON << 2) ; was B button pressed?
+	and B_BUTTON << 2 ; was B button pressed?
 	jr nz, .updateCursorPosition
 ; A button was pressed
 	ld a, [wCurrentMenuItem]
@@ -260,7 +260,7 @@ LinkMenu:
 	call DelayFrames
 	call LoadScreenTilesFromBuffer1
 	ld a, [wLinkMenuSelectionSendBuffer]
-	and (B_BUTTON << 2) ; was B button pressed?
+	and B_BUTTON << 2 ; was B button pressed?
 	jr nz, .choseCancel ; cancel if B pressed
 	ld a, [wCurrentMenuItem]
 	cp $2
