71cf972ea270a1fe6ed84e13ae41c588cf7a2dd6
Winxuser <jdownting31@gmail.com>
2025-04-27 19:02:13 +1000

    Add updated gym leader overworld sprites also update mews so it doesnt look like the fairy sprite

--- data/maps/objects/VermilionDock.asm
+++ data/maps/objects/VermilionDock.asm
@@ -8,6 +8,6 @@ VermilionDock_Object:
 	def_bg_events
 
 	def_object_events
-	object_event 21,  0, SPRITE_FAIRY, STAY, DOWN, 2, MEW, 50
+	object_event 21,  0, SPRITE_MEW, STAY, DOWN, 2, MEW, 50
 
 	def_warps_to VERMILION_DOCK
