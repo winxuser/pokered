d3416769a47fc301735ae61d43241d6bf23e2c42
Winxuser <jdownting31@gmail.com>
2025-06-26 09:45:00 +1000

    Infinite Use TMs

--- engine/items/item_effects.asm
+++ engine/items/item_effects.asm
@@ -2294,13 +2294,7 @@ ItemUseTMHM:
 	ld [wCurItem], a
 	pop af
 	ld [wWhichPokemon], a
-	ld a, b
-	and a
-	ret z
-	ld a, [wCurItem]
-	call IsItemHM
-	ret c
-	jp RemoveUsedItem
+	ret
 
 BootedUpTMText:
 	text_far _BootedUpTMText
