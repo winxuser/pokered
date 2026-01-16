7771cfdf84c66261c427045c446ef739ea7433b7
winxuser <jdownting31@gmail.com>
2026-01-16 14:59:45 +1000

    Fix Hall of Fame room
    Did not realise this had the Gym tileset, which had been changed a lot, so the room was glitched

--- data/maps/objects/HallOfFame.asm
+++ data/maps/objects/HallOfFame.asm
@@ -11,6 +11,6 @@ HallOfFame_Object:
 	def_bg_events
 
 	def_object_events
-	object_event  5,  2, SPRITE_OAK, STAY, DOWN, TEXT_HALLOFFAME_OAK
+	object_event  5,  1, SPRITE_OAK, STAY, DOWN, TEXT_HALLOFFAME_OAK
 
 	def_warps_to HALL_OF_FAME
