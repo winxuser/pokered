3bfa433dd0f967c28e591e0c30113f2e3648bc7d
winxuser <jdownting31@gmail.com>
2026-03-09 08:41:38 +1000

    Fix up some routes that i missed

--- data/maps/objects/Route10.asm
+++ data/maps/objects/Route10.asm
@@ -11,7 +11,7 @@ Route10_Object:
 
 	def_warp_events
 	warp_event 11, 19, ROCK_TUNNEL_POKECENTER, 1
-	warp_event  8, 17, ROCK_TUNNEL_1F, 1
+	warp_event  6, 17, ROCK_TUNNEL_1F, 1
 	warp_event  8, 53, ROCK_TUNNEL_1F, 3
 	warp_event  6, 39, POWER_PLANT, 1
 
--- data/maps/objects/Route5.asm
+++ data/maps/objects/Route5.asm
@@ -9,7 +9,7 @@ Route5_Object:
 	warp_event  9, 29, ROUTE_5_GATE, 3
 	warp_event 10, 33, ROUTE_5_GATE, 1
 	warp_event 17, 27, UNDERGROUND_PATH_ROUTE_5, 1
-	warp_event 10, 21, DAYCARE, 1
+	warp_event  9, 21, DAYCARE, 1
 
 	def_bg_events
 	bg_event 17, 29, TEXT_ROUTE5_UNDERGROUND_PATH_SIGN
